107ae4f957f0f61d95668a7db44b99a1e40230a6
Thalbu <tainanalbuquerque1@hotmail.com>
2022-05-16 08:25:53 -0300

    fix: realiza correcao de caminhos

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import {BrowserRouter} from 'react-router-dom'
-import Login from '../src/pages/Login'
-import Esqueceu_a_senha from './pages/Esqueceu_a_Senha'
-import Routes from './Routes'
-import api from './services/api';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Login from "../src/pages/Login";
+import EsqueceuASenha from "./pages/Esqueceu-A-Senha";
+import Routes from "./Routes";
+import api from "./services/api";
 
 function App() {
   return (
--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route } from 'react-router-dom';
-import Esqueceu_a_senha from "./pages/Esqueceu_a_Senha";
+import { Switch, Route } from "react-router-dom";
+import EsqueceuASenha from "./pages/Esqueceu-A-Senha/index";
 import Login from "./pages/Login";
 import CadastrarAluno from "./pages/Cadastrar-Aluno/cadastrar-aluno";
 import CadastrarEmpresa from "./pages/Cadastrar-Empresa/cadastrar-empresa";
@@ -9,36 +9,35 @@ import CadastroConfirmado from "./pages/cadastro_confirmado";
 import Home from "./pages/home/home";
 
 export default () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
 
-    return(
-        <Switch>
-            <Route exact path="/">
-                <Login/>
-            </Route>
+      <Route exact path="/password_recovery">
+        <EsqueceuASenha />
+      </Route>
 
-            <Route exact path="/password_recovery" >
-                <Esqueceu_a_senha/>
-            </Route>
+      <Route exact path="/cadastro">
+        <Cadastro />
+      </Route>
 
-            <Route exact path="/cadastro" >
-                <Cadastro/>
-            </Route>
-            
-            <Route exact path="/cadastrar_aluno" >
-                <CadastrarAluno/>
-            </Route>
+      <Route exact path="/cadastrar_aluno">
+        <CadastrarAluno />
+      </Route>
 
-            <Route exact path="/cadastrar_empresa" >
-                <CadastrarEmpresa/>
-            </Route>
+      <Route exact path="/cadastrar_empresa">
+        <CadastrarEmpresa />
+      </Route>
 
-            <Route exact path="/cadastro_confirmado">
-                <CadastroConfirmado/>
-            </Route>
+      <Route exact path="/cadastro_confirmado">
+        <CadastroConfirmado />
+      </Route>
 
-            <Route exact path="/home" >
-                <Home/>
-            </Route>
-        </Switch>
-    );
-}
+      <Route exact path="/home">
+        <Home />
+      </Route>
+    </Switch>
+  );
+};
