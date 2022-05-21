d95ccb90a54d6f9d84ba9e304b91e5d24c2510ec
Thalbu <tainanalbuquerque1@hotmail.com>
2022-05-21 16:20:22 -0300

    corrige warning

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { /*Switch,*/ Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import EsqueceuASenha from "./pages/Esqueceu-A-Senha/index";
 import Login from "./pages/Login";
 import CadastrarAluno from "./pages/Cadastrar-Aluno/cadastrar-aluno";
@@ -8,28 +8,37 @@ import Cadastro from "./pages/cadastro";
 import CadastroConfirmado from "./pages/cadastro_confirmado";
 import Home from "./pages/home/home";
 
-export default (
-  <>
-    <Route exact path="/">
-      <Login />
-    </Route>
-    <Route exact path="/password_recovery">
-      <EsqueceuASenha />
-    </Route>
-    <Route exact path="/cadastro">
-      <Cadastro />
-    </Route>
-    <Route exact path="/cadastrar_aluno">
-      <CadastrarAluno />
-    </Route>
-    <Route exact path="/cadastrar_empresa">
-      <CadastrarEmpresa />
-    </Route>
-    <Route exact path="/cadastro_confirmado">
-      <CadastroConfirmado />
-    </Route>
-    <Route exact path="/home">
-      <Home />
-    </Route>
-  </>
-);
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default () => {
+  return (
+    <>
+      <Route exact path="/">
+        <Login />
+      </Route>
+
+      <Route exact path="/password_recovery">
+        <EsqueceuASenha />
+      </Route>
+
+      <Route exact path="/cadastro">
+        <Cadastro />
+      </Route>
+
+      <Route exact path="/cadastrar_aluno">
+        <CadastrarAluno />
+      </Route>
+
+      <Route exact path="/cadastrar_empresa">
+        <CadastrarEmpresa />
+      </Route>
+
+      <Route exact path="/cadastro_confirmado">
+        <CadastroConfirmado />
+      </Route>
+
+      <Route exact path="/home">
+        <Home />
+      </Route>
+    </>
+  );
+};
