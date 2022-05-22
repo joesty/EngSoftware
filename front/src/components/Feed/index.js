5cea39f694bf65046a68442701a72ca5c43812c3
Artur Mayrink <arturmayrink@hotmail.com>
2022-05-22 20:38:44 -0300

    Adiciona título do feed

--- front/src/components/Feed/index.js
+++ front/src/components/Feed/index.js
@@ -3,6 +3,9 @@ import "./feed.css";
 export default function Feed(props) {
   return (
     <>
+    <div className="titulo">
+        <h2>Encontre sua vaga:</h2>
+    </div>
       {props.vagas.map((vaga) => (
         <div className="vaga">
           <h1>
