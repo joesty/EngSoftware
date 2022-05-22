f8255e6b1aed5ebea12f52c358667009c0dad344
Thalbu <tainanalbuquerque1@hotmail.com>
2022-05-22 19:08:56 -0300

    altera CSS

--- front/src/models/student.js
+++ front/src/models/student.js
@@ -1,31 +1,34 @@
-import React from "react";
+// import React from "react";
 import api from "../services/api";
 
-class Student{
-    constructor(name, last_name, cpf, tel, course, email, password){
-        this.name = name;
-        this.last_name = last_name;
-        this.cpf = cpf;
-        this.tel = tel;
-        this.course = course;
-        this.email = email;
-        this.password = password;   
-    }
-    async createUser(){
-        api.post("/students/",{ 
-            name: this.name,
-            last_name: this.last_name,
-            email: this.email,
-            course: this.course,
-            password: this.password,
-            cpf: this.cpf,
-            phone_number: this.tel
-        }).then(function (response){
-            console.log(response)
-        }).catch(function (error){
-            console.log(error)
-        });
-    };
+class Student {
+  constructor(name, last_name, cpf, tel, course, email, password) {
+    this.name = name;
+    this.last_name = last_name;
+    this.cpf = cpf;
+    this.tel = tel;
+    this.course = course;
+    this.email = email;
+    this.password = password;
+  }
+  async createUser() {
+    api
+      .post("/students/", {
+        name: this.name,
+        last_name: this.last_name,
+        email: this.email,
+        course: this.course,
+        password: this.password,
+        cpf: this.cpf,
+        phone_number: this.tel,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
 
 export default Student;
