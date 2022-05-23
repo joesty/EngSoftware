bf5ea5a945ab15e661962be3f004a1e71f21c418
Artur Mayrink <arturmayrink@hotmail.com>
2022-05-22 21:04:02 -0300

    teste

--- front/src/pages/Feed/feed.js
+++ front/src/pages/Feed/feed.js
@@ -50,7 +50,7 @@ const arrayVagas = [
   },
   {
     cargo: "Engenheiro de Software",
-    empresa: "Google",
+    empresa: "Oi",
     localizacao: "Belo Horizonte - MG",
     descricao:
       "Estamos procurando um Engenheiro de Software com interesse especial em projetar, desenvolver e instalar soluções de software. As responsabilidades do Engenheiro de Software incluem reunir requisitos do usuário, definir funcionalidade de sistemas e escrever códigos em várias linguagens, como Java, Ruby on Rails, ou linguagens de programação .NET (por exemplo, C++ ou JScript.NET.) O candidato ideal deve ter familiaridade com o ciclo de vida de desenvolvimento de softwares (SDLC), desde a análise preliminar do sistema até as fases de teste e implantação. Basicamente, a função do Engenheiro de Software é desenvolver softwares de alta qualidade, inovadores e completamente funcionais em conformidade com os padrões de codificação e projeto técnico.",
